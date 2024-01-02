237c818ac0d87ae9575f6911f85b7ba036889ff4
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2024-01-02 22:47:35 +0300

    main

---
+++ backend/target/debug/deps/backend-9c3052c04d3b472e.d
@@ -0,0 +1,5 @@
+/Users/leonfarafonov/Projects/Public/awesome-todo/backend/target/debug/deps/libbackend-9c3052c04d3b472e.rmeta: src/main.rs
+
+/Users/leonfarafonov/Projects/Public/awesome-todo/backend/target/debug/deps/backend-9c3052c04d3b472e.d: src/main.rs
+
+src/main.rs:
